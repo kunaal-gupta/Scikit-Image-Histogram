7a82af9fca7bc17005fb329c84c79216cd00614f
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2023-01-19 15:30:50 -0700

    refactored image reading in grayscale

--- test4.py
+++ test4.py
@@ -1,8 +1,43 @@
-n = int(input())
-flag = 1/(n-1)
-bin_len= [0]
+import numpy as np
+import imhist as imhist
+from skimage import io
 
-for i in range(n-1):
-    bin_len.append(bin_len[-1]+flag)
-print(bin_len)
+import matplotlib
 
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+image = io.imread("day.jpg")
+image = image.ravel()
+
+
+def get_histogram(image, bins):
+    # array with size of bins, set to zeros
+    histogram = np.zeros(bins)
+
+    # loop through pixels and sum up counts of pixels
+    for pixel in image:
+        histogram[int(pixel)] += 1
+
+    # return our final result
+    return histogram
+
+
+hist = get_histogram(image, 256)
+plt.plot(hist)
+plt.show()
+
+def cumsum(a):
+    a = iter(a)
+    b = [next(a)]
+    for i in a:
+        b.append(b[-1] + i)
+    return np.array(b)
+
+
+# execute the fn
+cs = cumsum(hist)
+
+# display the result
+# plt.plot(cs)
+# plt.show()
